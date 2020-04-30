60b3b3046a0fb251ff138a027ce5a2099b37c7d8
Arina Melly Aziz Safitri <50207732+arnmly@users.noreply.github.com>
2020-04-30 23:22:12 +0700

    Update and rename SPBU Maps to MapsActivity.java

--- MapsActivity.java
+++ MapsActivity.java
@@ -188,40 +188,6 @@ public class MapsActivity extends FragmentActivity implements
 }
 
 
-<!-- ----------- AndroidManifest.xml ------------- -->
-
-<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="com.example.maps">
-
-    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
-
-    <application
-        android:allowBackup="true"
-        android:icon="@mipmap/spbu"
-        android:label="@string/app_name"
-        android:roundIcon="@mipmap/spbu"
-        android:supportsRtl="true"
-        android:theme="@style/AppTheme">
-
-        <meta-data
-            android:name="com.google.android.geo.API_KEY"
-            android:value="@string/google_maps_key" />
-
-        <activity
-            android:name=".MapsActivity"
-            android:label="SPBU Maps">
-            <intent-filter>
-                <action android:name="android.intent.action.MAIN" />
-
-                <category android:name="android.intent.category.LAUNCHER" />
-            </intent-filter>
-        </activity>
-    </application>
-
-</manifest>
-
-
 <!-- ---------- google_maps_api.xml --------- -->
 
 <resources>
